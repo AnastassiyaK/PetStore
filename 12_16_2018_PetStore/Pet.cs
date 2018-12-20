dabb70a3eaa1b5cef4f69072d37ba9dd4eb948a7
KorotkovaAna <nastyakorotkova13@gmail.com>
2018-12-21 02:26:51 +0300

    fixed

---
+++ 12_16_2018_PetStore/Pet.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_16_2018_PetStore
+{
+    class Pet
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int id { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string name{ get; set; }
+        [JsonProperty(PropertyName = "photoUrls")]
+        public ICollection<string> photoUrls { get; set; }
+        [JsonProperty(PropertyName = "status")]
+        public string status { get; set; }
+        
+            
+    }
+}
